88b5e3fd69d93c8eb579179712d70f41c20c774d
pablorino <pablorinoflores@gmail.com>
2024-03-21 19:07:46 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MEDAC</title>
-        @viteReactRefresh
-        @vite('resources/js/app.js')
-    </head>
-    <body class="antialiased">
-       <div id='root'></div>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MEDAC</title>
+    @viteReactRefresh
+    @vite('resources/js/app.js')
+</head>
+
+<body class="antialiased">
+    <div id='root'></div>
+    <h1>Hola</h1>
+</body>
+
 </html>
